70d2f41683ae93a81805a61afd1594e671d0a464
izuddin41086@gmail.com <izuddin41086@gmail.com>
2020-11-30 11:19:26 +0700

    WA

--- controllers/web/orders.controller.js
+++ controllers/web/orders.controller.js
@@ -29,14 +29,14 @@ exports.confirmOrder = async (req, res, next) => {
     var bodyPost = req.body
     bodyPost.total_order = bodyPost.jumlah_order * Product.price
 
-    var waOrder = `Assalamualaykum ${cProfile.name}, mau order nih 
-    Nama Produk: ${Product.title} 
-    Nama Pemesan: ${bodyPost.cust_name} 
-    Email Pemesan: ${bodyPost.email} 
-    Telepon: ${bodyPost.phone} 
-    Tgl Pengiriman: ${bodyPost.date_picker} 
-    Waktu: ${bodyPost.time_picker} 
-    Alamat Pengiriman: ${bodyPost.address} 
+    var waOrder = `Assalamualaykum ${cProfile.name}, mau order nih %0D%0A
+    Nama Produk: ${Product.title} %0D%0A
+    Nama Pemesan: ${bodyPost.cust_name} %0D%0A
+    Email Pemesan: ${bodyPost.email} %0D%0A
+    Telepon: ${bodyPost.phone} %0D%0A
+    Tgl Pengiriman: ${bodyPost.date_picker} %0D%0A
+    Waktu: ${bodyPost.time_picker} %0D%0A
+    Alamat Pengiriman: ${bodyPost.address} %0D%0A
     Total Harga: ${Intl.NumberFormat().format(bodyPost.total_order)}`;
 
     var waOrderTemplate = encodeURIComponent(waOrder)
