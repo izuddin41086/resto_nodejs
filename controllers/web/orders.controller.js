77b1d6d77807467e64ccbe40e655b2a1a1aba65f
izuddin41086@gmail.com <izuddin41086@gmail.com>
2020-11-30 14:29:38 +0700

    Fix WA

--- controllers/web/orders.controller.js
+++ controllers/web/orders.controller.js
@@ -29,14 +29,14 @@ exports.confirmOrder = async (req, res, next) => {
     var bodyPost = req.body
     bodyPost.total_order = bodyPost.jumlah_order * Product.price
 
-    var waOrder = `Assalamualaykum ${cProfile.name}, mau order nih \r\n
-    Nama Produk: ${Product.title} \r\n
-    Nama Pemesan: ${bodyPost.cust_name} \r\n
-    Email Pemesan: ${bodyPost.email} \r\n
-    Telepon: ${bodyPost.phone} \r\n
-    Tgl Pengiriman: ${bodyPost.date_picker} \r\n
-    Waktu: ${bodyPost.time_picker} \r\n
-    Alamat Pengiriman: ${bodyPost.address} \r\n
+    var waOrder = `Assalamualaykum ${cProfile.name}, mau order nih
+    Nama Produk: ${Product.title}
+    Nama Pemesan: ${bodyPost.cust_name}
+    Email Pemesan: ${bodyPost.email}
+    Telepon: ${bodyPost.phone}
+    Tgl Pengiriman: ${bodyPost.date_picker}
+    Waktu: ${bodyPost.time_picker}
+    Alamat Pengiriman: ${bodyPost.address}
     Total Harga: ${Intl.NumberFormat().format(bodyPost.total_order)}`;
 
     var waOrderTemplate = encodeURIComponent(waOrder);
