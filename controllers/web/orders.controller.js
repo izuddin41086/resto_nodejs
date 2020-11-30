fc0cc35c7d9c99582fd16afe25e28d58e858f86b
izuddin41086@gmail.com <izuddin41086@gmail.com>
2020-11-30 16:44:29 +0700

    Fix template

--- controllers/web/orders.controller.js
+++ controllers/web/orders.controller.js
@@ -29,8 +29,7 @@ exports.confirmOrder = async (req, res, next) => {
     var bodyPost = req.body
     bodyPost.total_order = bodyPost.jumlah_order * Product.price
 
-    var waOrder = "Assalamualaykum " + cProfile.name + "\r\n";
-    waOrder += "Saya pesan dengan detail sbb:\r\n";
+    var waOrder = "Assalamualaykum " + cProfile.name + ", saya pesan dengan rincian sbb:\r\n\r\n";
     waOrder += "Nama Produk: " + Product.title + "\r\n";
     waOrder += "Nama Pemesan: " + bodyPost.cust_name + "\r\n";
     waOrder += "Email Pemesan: " + bodyPost.email + "\r\n";
