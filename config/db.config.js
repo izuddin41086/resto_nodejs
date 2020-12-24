682fd7b86d818b0642ba9768a1af06f37dff4dd9
Izuddin <mrizuddin7@gmail.com>
2020-12-24 13:22:27 +0700

    Last Commit

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    //url: "mongodb://localhost:27017/bezkoder_db"
-    url: "mongodb+srv://someuser:12345@joetcluster.pip9s.mongodb.net/one-store?retryWrites=true&w=majority"
+    url: "mongodb://localhost:27017/bezkoder_db"
 };
